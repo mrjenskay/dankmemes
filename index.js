81bd62a4f51f6862e90d80f2579fcd4de0bbb607
root <11043@hageveld.nl>
2016-05-25 17:47:23 +0200

    Screen

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.write('<div id="header"><p class="hak">5 memes</p></div><center>');
+document.write('<div id="header"><p class="hak">5 memes</p></div><center><div class=\"container-fluid\">');
   var text = '{"memes":[' +
   '{"name":"Smoke weed everyday!!!","description":"This is Tijmen, Tijmen is a stoner." ,"file":"img/1.png"},' +
   '{"name":"Smoke weed everyday!!!","description":"This is Tijmen, Tijmen is a stoner.", "file":"img/1.png"},' +
@@ -15,7 +15,7 @@ document.write('<div id="header"><p class="hak">5 memes</p></div><center>');
 
   obj = JSON.parse(text);
 $i = 0;while ($i < 5){
-  document.write("<div class=\"col-md-5 jumbotron\"><h1>"+obj.memes[$i].name+"</h1><br><img src='"+obj.memes[$i].file+"' /><br><br><p>"+obj.memes[$i].description+"</p><br><a href=\""+obj.memes[$i].file+"\" download><button class=\"btn btn-success\">DOWNLOAD</button></a><button class=\"btn btn-warning\">SHARE</button></div><br>");$i++;}
+  document.write("<div class=\"col-md-5 jumbotron\"><h1>"+obj.memes[$i].name+"</h1><br><img class=\"meme\" src='"+obj.memes[$i].file+"' /><br><br><p>"+obj.memes[$i].description+"</p><br><a href=\""+obj.memes[$i].file+"\" download><button class=\"btn btn-success\">DOWNLOAD</button></a><button class=\"btn btn-warning\">SHARE</button></div><br>");$i++;}
 
 function more() {
 $j = 5;while ($j < 10){
