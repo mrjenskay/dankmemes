79c8d69aa9c7bb507cf6238c387cba75b067eea4
root <11043@hageveld.nl>
2016-05-25 20:52:59 +0200

    ++

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ document.write('<div id="header"><p class="hak">5 memes</p></div><center><div cl
 
   obj = JSON.parse(text);
 for(i=0;i<5;i++){
-  document.write("<div class=\"col-md-5 jumbotron\"><a href='page.php?id="+i+"'><h1>"+obj.memes[i].name+"</h1></a><br><img class=\"meme\" src='"+obj.memes[i].file+"' /><br><br><p>"+obj.memes[i].description+"</p><br><a href=\""+obj.memes[i].file+"\" download><button class=\"btn btn-success\">DOWNLOAD</button></a><button class=\"btn btn-warning\">SHARE</button></div><br>");i++;
+  document.write("<div class=\"col-md-5 jumbotron\"><a href='page.php?id="+i+"'><h1>"+obj.memes[i].name+"</h1></a><br><img class=\"meme\" src='"+obj.memes[i].file+"' /><br><br><p>"+obj.memes[i].description+"</p><br><a href=\""+obj.memes[i].file+"\" download><button class=\"btn btn-success\">DOWNLOAD</button></a><button class=\"btn btn-warning\">SHARE</button></div><br>");
 }
