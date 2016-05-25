62d7c11249ff793296b23e72990ad1550d8b8fe6
root <11043@hageveld.nl>
2016-05-25 14:16:12 +0200

    Add description

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ $i = 0;while ($i < 10){
   '{"name":"Smoke weed everyday!!!","description":"This is Tijmen, Tijmen is a stoner.", "file":"img/1.png"}]}';
 
   obj = JSON.parse(text);
-  document.write("<div class=\"col-md-5 jumbotron\"><h1>"+obj.memes[$i].name+"</h1><br><img src='"+obj.memes[$i].file+"' /><br><br><button class=\"btn btn-success\">DOWNLOAD</button><button class=\"btn btn-warning\">SHARE</button></div><br>");$i++;}
+  document.write("<div class=\"col-md-5 jumbotron\"><h1>"+obj.memes[$i].name+"</h1><br><img src='"+obj.memes[$i].file+"' /><br><br><p>"+obj.memes[$i].description+"</p><br><button class=\"btn btn-success\">DOWNLOAD</button><button class=\"btn btn-warning\">SHARE</button></div><br>");$i++;}
