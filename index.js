6f1d1fa719caa710b33e3cc4a1e5b61028f2d1d5
root <11043@hageveld.nl>
2016-05-25 14:08:16 +0200

    Update

---
+++ index.js
@@ -0,0 +1,18 @@
+document.write('<div id="header"><p class="hak">10 memes</p></div><center>');
+$i = 0;while ($i < 10){
+  var text = '{"memes":[' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"},' +
+  '{"name":"Smoke weed everyday!!!", "file":"img/1.png"}]}';
+
+  obj = JSON.parse(text);
+  document.write("<div class=\"col-md-5 jumbotron\"><h1>"+obj.memes[$i].name+"</h1><br><img src='"+obj.memes[$i].file+"' /><br><br><button class=\"btn btn-success\">DOWNLOAD</button><button class=\"btn btn-warning\">SHARE</button></div><br>");$i++;}
